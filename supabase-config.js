16cbccfec16a77c5691fadd577a57b8ceb5248a9
mohamadgad83 <mood20800@gmail.com>
2026-05-29 23:51:31 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = 'https://duxqpoooowiqzcedmmoh.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1eHFwb29vd2l3cXpjZWRtbW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MjY0NTYsImV4cCI6MjA2MTUwMjQ1Nn0.K1TVi7W2XwHmL5jBfXtqOsWnObe5JqMNvj-3fUpYSoA'
+const supabaseUrl = 'https://onrnecgazlrdzdywauev.supabase.co'
+const supabaseAnonKey = 'sb_publishable_Rpw7pfPokf0BkEQQlkAFqA_QFnHKov4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
